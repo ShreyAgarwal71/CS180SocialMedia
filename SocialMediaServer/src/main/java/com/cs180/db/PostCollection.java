1439f3c33d3e1e68f9820798313a20a936d2a856
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-10-30 16:06:06 -0400

    These are probably war crimes

--- SocialMediaServer/src/main/java/com/cs180/db/PostCollection.java
+++ SocialMediaServer/src/main/java/com/cs180/db/PostCollection.java
@@ -1,5 +1,32 @@
 package com.cs180.db;
 
-public class PostCollection implements Collection {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class PostCollection implements Collection {
+	private final String fileName;
+	private ArrayList<Post> posts;
+
+	public PostCollection(String fileName) {
+		this.fileName = fileName;
+		Object[] o = this.readData(fileName);
+		posts = new ArrayList<>(Arrays.asList((Post[]) o));
+	}
+
+	// Wrapper for writeData for easier use
+	public boolean writePosts() {
+		Object[] temp = new Object[posts.size()];
+		posts.toArray(temp);
+		return this.writeData(fileName, temp);
+	}
+
+	@Override
+	public Object[] readData(String fileName) {
+		return Collection.super.readData(fileName);
+	}
+
+	@Override
+	public boolean writeData(String fileName, Object[] data) {
+		return Collection.super.writeData(fileName, data);
+	}
 }
--- SocialMediaServer/src/main/java/com/cs180/db/UserCollection.java
+++ SocialMediaServer/src/main/java/com/cs180/db/UserCollection.java
@@ -1,4 +1,32 @@
 package com.cs180.db;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UserCollection implements Collection {
+	private final String fileName;
+	private ArrayList<User> users;
+
+	public UserCollection(String fileName) {
+		this.fileName = fileName;
+		Object[] o = this.readData(fileName);
+		users = new ArrayList<>(Arrays.asList((User[]) o));
+	}
+
+	// Wrapper for writeData for easier use
+	public boolean writePosts() {
+		Object[] temp = new Object[users.size()];
+		users.toArray(temp);
+		return this.writeData(fileName, temp);
+	}
+
+	@Override
+	public Object[] readData(String fileName) {
+		return Collection.super.readData(fileName);
+	}
+
+	@Override
+	public boolean writeData(String fileName, Object[] data) {
+		return Collection.super.writeData(fileName, data);
+	}
 }
