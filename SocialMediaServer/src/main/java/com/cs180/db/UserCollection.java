c4a5a149466e68160056ce1c128d36a9726ae416
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-01 10:30:12 -0400

    Implemented UserCollection

--- SocialMediaServer/src/main/java/com/cs180/db/UserCollection.java
+++ SocialMediaServer/src/main/java/com/cs180/db/UserCollection.java
@@ -20,10 +20,89 @@ public class UserCollection implements Collection {
 		users = new ArrayList<>(Arrays.asList((User[]) o));
 	}
 
-	// Wrapper for writeData for easier use
-	public boolean writePosts() {
+	/**
+	 * Wrapper method for the Collection interface's writeData method.
+	 * This methods fills up Collection.writeData()'s parameters automatically.
+	 * Returns true on success, false on fail.
+	 * @return exitCode
+	 */
+	public boolean writeUsers() {
 		Object[] temp = new Object[users.size()];
 		users.toArray(temp);
 		return this.writeData(fileName, temp);
 	}
+	
+	@Override
+	public int indexOf(Object obj) {
+		int index = -1;
+		if (!(obj instanceof User))
+			return index;
+		User u = (User) obj;
+
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).equals(u)) {
+				index = i;
+				break;
+			}
+		}
+		
+		return index;
+	}
+
+	@Override
+	public boolean updateElement(Object target, Object newObj) {
+		boolean exitCode = false;
+
+		int index = this.indexOf(target);
+		if (index == -1)
+			return exitCode;
+
+		if (index < users.size() && index >= 0) {
+			users.set(index, (User) newObj);
+			exitCode = this.writeUsers();
+		}
+		
+		return exitCode;
+	}
+
+	@Override
+	public boolean updateElement(int index, Object newObj) {
+		boolean exitCode = false;
+		if (index >= users.size() || index < 0 || !(newObj instanceof User))
+			return exitCode;
+
+		users.set(index, (User) newObj);
+		exitCode = this.writeUsers();
+			
+		return exitCode;
+	}
+
+	@Override
+	public boolean removeElement(Object obj) {
+		boolean exitCode = false;
+
+		int index = this.indexOf(obj);
+		if (index == -1)
+			return exitCode;
+
+		if (index < users.size() && index >= 0) {
+			users.remove(index);
+			exitCode = this.writeUsers();
+		}
+
+		return exitCode;
+	}
+
+	@Override
+	public boolean removeElement(int index) {
+		boolean exitCode = false;
+		if (index >= users.size() || index < 0)
+			return exitCode;
+
+		users.remove(index);
+		exitCode = this.writeUsers();
+			
+		return exitCode;
+	}
+
 }
