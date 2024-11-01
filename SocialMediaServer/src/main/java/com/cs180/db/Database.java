e60d7c7a27d6c6159ddce396df138c14dfedd5ac
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-01 12:38:35 -0400

    Created Database Constructor

--- SocialMediaServer/src/main/java/com/cs180/db/Database.java
+++ SocialMediaServer/src/main/java/com/cs180/db/Database.java
@@ -1,3 +1,25 @@
 package com.cs180.db;
 
-public class Database {}
+// TODO: Javadoc
+public class Database {
+	private static final String userFile = "users.txt";
+	private static final String postFile = "posts.txt";
+	private static final String commentFile = "comments.txt";
+	private final UserCollection uc;
+	private final PostCollection pc;
+	private final CommentCollection cc;
+	
+	public Database() {
+		uc = new UserCollection(userFile);
+		pc = new PostCollection(postFile);
+		cc = new CommentCollection(commentFile);
+	}
+
+	/**
+	 * I suggest making each collection singleton have its own socket 
+	 * in its own thread and thus achieveing concurrenty.
+	 * This would mean the implementation of this class would be left for phase 2.
+	 *
+	 * TODO: Implement the class
+	 */
+}
