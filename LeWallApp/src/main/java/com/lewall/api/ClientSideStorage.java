69c9d99b2f700afe29faaa8c98d2d2d7b8b39e70
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-14 23:09:04 -0500

    Implemented Client Side Data Read/Write

--- LeWallApp/src/main/java/com/lewall/api/ClientSideStorage.java
+++ LeWallApp/src/main/java/com/lewall/api/ClientSideStorage.java
@@ -1,5 +1,13 @@
 package com.lewall.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
 import org.apache.logging.log4j.LogManager;
@@ -17,27 +25,73 @@ import com.google.gson.Gson;
  */
 public class ClientSideStorage {
 	private static Logger logger = LogManager.getLogger(ClientSideStorage.class);
-	private static HashMap<String, String> storage = new HashMap<>();
+	private static HashMap<String, String> storage;
 
     private static final Gson gson = new GsonBuilder().create();
 
 	/*
-	 * TODO: Load data if available, else initialize new stuff if
+	 * Reads data from the disk
+	 *
+	 * @return exitCode
 	 */
-	public ClientSideStorage() {
+	@SuppressWarnings("unchecked")
+	public boolean readFromDisk() {
+		String path = getOSDataBasePath().toString();
+		
+		File f = new File(path);
+		if (!f.exists()){
+			try {
+				storage = new HashMap<>();
+				f.createNewFile();
+			} catch (IOException e) {
+				logger.error(e);
+				return false;
+			}
+		} else {
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f))) {
+				// TODO: Test this!
+				storage = (HashMap<String, String>) ois.readObject();
+			} catch (Exception e) {
+				logger.error(e);
+				return false;
+			}
+		}
 
+		return true;
 	}
 
 	/*
-	 * TODO: Save state to disk
+	 * Saves current state to disk
+	 *
 	 * @return exitCode
 	 */
-	public boolean saveToDisk() {
-		return false;
+	public static boolean saveToDisk() {
+		if (storage == null || storage.isEmpty()) {
+			logger.error("Cannot save unititialized client side data!");
+			return false;
+		}
+
+		String path = getOSDataBasePath().toString();
+		
+		File f = new File(path);
+		if (!f.exists()){
+			logger.error("Client side storage file does not exist!");
+			return false;
+		}
+
+		try (ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(f))) {
+			ous.writeObject(storage);
+		} catch (Exception e) {
+			logger.error(e);
+			return false;
+		}
+
+		return true;
 	}
 	
 	/*
 	 * Fetches the requested data from the storage HasMap in JSON format
+	 *
 	 * @param key
 	 * @return val
 	 */
@@ -52,6 +106,24 @@ public class ClientSideStorage {
 		}
 
 		return val;
+	}
+
+	/*
+	 * Gets the storage file location based on the OS
+	 * @return path
+	 */
+	private static Path getOSDataBasePath() {
+		String os = System.getProperty("os.name").toLowerCase();
+
+		if (os.contains("win")) {
+			return Paths.get(System.getenv("APPDATA"), "LeWall", "data.txt");
+		} else if (os.contains("mac")) {
+			return Paths.get(System.getProperty("user.home"), "/Library/Application Support", "LeWall", "data.txt");
+		} else if (os.contains("nix") || os.contains("nux")) {
+			return Paths.get(System.getProperty("user.home"), ".lewall", "data.txt");
+		} else {
+			return Paths.get(System.getProperty("user.home"), "LeWall", "data.txt");
+		}
 	}
 }
 
