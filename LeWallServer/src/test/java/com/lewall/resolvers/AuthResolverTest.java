ef933cf5bcce387fb0138003a26f8a7424aaed7e
Ates <88388753+AtesIsf@users.noreply.github.com>
2024-11-16 18:31:02 -0500

    Deleted unused imports

--- LeWallServer/src/test/java/com/lewall/resolvers/AuthResolverTest.java
+++ LeWallServer/src/test/java/com/lewall/resolvers/AuthResolverTest.java
@@ -1,8 +1,5 @@
 package com.lewall.resolvers;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
